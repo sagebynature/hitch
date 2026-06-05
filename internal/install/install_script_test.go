dd0b5613b3b8b3fd21f16d15c0d3b8105e5a95d1
Sage Choi <sage.choi@gmail.com>
2026-06-05 17:08:06 -0400

    fix: quote manual installer URL

--- internal/install/install_script_test.go
+++ internal/install/install_script_test.go
@@ -104,7 +104,7 @@ func TestSourceInstallerInstallModes(t *testing.T) {
 			wantOutput: []string{
 				"Installed Hitch client to",
 				"Run hook setup with:",
-				"--url http://127.0.0.1:9876",
+				"--url 'http://127.0.0.1:9876'",
 				"hitch-client install",
 			},
 		},
--- scripts/install.sh
+++ scripts/install.sh
@@ -109,7 +109,7 @@ server_url_command() {
 
 hook_setup_command() {
   if [ -n "$HITCH_URL" ]; then
-    printf '%s/hitch-client install --url %s' "$HITCH_INSTALL_DIR" "$HITCH_URL"
+    printf '%s/hitch-client install --url '\''%s'\''' "$HITCH_INSTALL_DIR" "$HITCH_URL"
   else
     printf '%s/hitch-client install' "$HITCH_INSTALL_DIR"
   fi
